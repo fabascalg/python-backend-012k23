33c657bba67c1a8eb94ecbef40131aa88cb2d436
fabascalg <38880768+fabascalg@users.noreply.github.com>
2023-01-09 21:39:47 +0100

    curso Python Brais Moure 2k23

---
+++ Backend/FastAPI/type_hints.py
@@ -0,0 +1,15 @@
+my_string_variable = "mi variable tipo cadena"
+print(my_string_variable)
+print(type(my_string_variable))
+
+my_int_variable = 5
+print(my_int_variable)
+print(type(my_int_variable))
+
+cadena: str = "variable de tipo String"
+print(cadena)
+print(type(cadena))
+
+cadena = 5
+print(cadena)
+print(type(cadena))
