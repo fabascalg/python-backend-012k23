b0bb09f6ce6e25e852cdfdc2592769fc61f2ca40
fabascalg <38880768+fabascalg@users.noreply.github.com>
2023-01-11 19:31:49 +0100

    "primera clase y array de instancias de la clase"

--- Backend/FastAPI/users.py
+++ Backend/FastAPI/users.py
@@ -1,13 +1,31 @@
 from fastapi import FastAPI
 
+from pydantic import BaseModel
+
 app = FastAPI()
 
 # inicia el server: uvicorn users:app --reload
 
+# Entidad user
+class User(BaseModel):
+    name: str
+    surname: str
+    url: str
+    age: int
 
-@app.get("/users")
+users = [User(name = "Brais",surname = "Moure", url = "https://moure.dev",age = 35),
+         User(name = "Pepe",surname = "González",url = "https://www.auyantepui.com",age = 40),
+         User(name = "Gema",surname = "Guerra",url = "https://www.valoren.es",age = 30)]    
+
+
+@app.get("/usuariosjson")
+async def jsonusuarios():
+    return users
+
+@app.get("/usuarios")
 async def usuarios():
-    return [{"name": "Brais", "surname": "Moure", "url": "https://moure.dev"},
-            {"name": "Pepe", "surname": "González",
-                "url": "https://www.auyantepui.com"},
-            {"name": "Gema", "surname": "Guerra", "url": "https://www.valoren.es"}]
+    return [{"name": "Gema", "surname": "Guerra", "url": "https://www.valoren.es", "age": 40},
+            {"name": "Pepe", "surname": "González", 
+                "url": "https://www.auyantepui.com", "age": 30},
+            {"name": "Brais", "surname": "Moure", "url": "https://moure.dev", "age": 20}]
+
