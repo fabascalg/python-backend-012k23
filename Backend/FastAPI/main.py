8fea5f687e409854d6457b437518112e16f2b038
fabascalg <38880768+fabascalg@users.noreply.github.com>
2023-01-11 18:41:23 +0100

    "segundo paso..."

--- Backend/FastAPI/main.py
+++ Backend/FastAPI/main.py
@@ -2,8 +2,24 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+# mapeo a raiz
 @app.get("/")
 def root():
-    return {"message": "¡Hola FastAPI!"}
+    return "¡Hola FastAPI! Aquí probando --reload"
+
+# mapeo a /url
+@app.get("/url")
+def url():
+    return {"url": "https://mouredev.com/python"}
+
+# Iniciar el server: uvicorn main:app --reload
+# Detener el server: CTRL+C
+
+# Documentación con Swagger: http://127.0.0.1:8000/docs
+# Documentación con Redocly: http://127.0.0.1:8000/redoc
+
+# PostMan
+
+
 
 
