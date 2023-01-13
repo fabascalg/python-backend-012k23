4376102c2e13a411ec0bc488096b10c6b621ca90
fabascalg <38880768+fabascalg@users.noreply.github.com>
2023-01-13 13:49:40 +0100

    "peticiones path y query"

--- Backend/FastAPI/users.py
+++ Backend/FastAPI/users.py
@@ -8,19 +8,20 @@ app = FastAPI()
 
 # Entidad user
 class User(BaseModel):
+    id: int
     name: str
     surname: str
     url: str
     age: int
 
-users = [User(name = "Brais",surname = "Moure", url = "https://moure.dev",age = 35),
-         User(name = "Pepe",surname = "González",url = "https://www.auyantepui.com",age = 40),
-         User(name = "Gema",surname = "Guerra",url = "https://www.valoren.es",age = 30)]    
+users_list = [User(id = 1, name = "Brais",surname = "Moure", url = "https://moure.dev",age = 35),
+         User(id = 2, name = "Pepe",surname = "González",url = "https://www.auyantepui.com",age = 40),
+         User(id = 3, name = "Gema",surname = "Guerra",url = "https://www.valoren.es",age = 30)]    
 
 
 @app.get("/usuariosjson")
 async def jsonusuarios():
-    return users
+    return users_list
 
 @app.get("/usuarios")
 async def usuarios():
@@ -29,3 +30,28 @@ async def usuarios():
                 "url": "https://www.auyantepui.com", "age": 30},
             {"name": "Brais", "surname": "Moure", "url": "https://moure.dev", "age": 20}]
 
+# se puede escribir con el mismo nombre, el servidor distingue la 
+# petición según sea por path o por query
+# ¿¿¿aquí también se llama sobrecarga???
+
+# petición por path
+@app.get("/usuario/{id}")
+async def usuario(id: int):
+    return search_user(id)
+
+
+# petición por query
+# ej: http://127.0.0.1:8000/usuarioconsulta/?id=3
+# userquery es como lo ha llamado Moure...
+@app.get("/usuario/")
+async def usuario(id: int):
+    return search_user(id)
+
+
+# la misma función trabaja para petición por path de id o por query...
+def search_user(id: int):
+    users = filter(lambda user: user.id == id, users_list)
+    try:
+        return list(users)[0]
+    except:
+        return {"error": "no se ha encontrado el usuario"}
